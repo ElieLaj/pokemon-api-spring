8014487f5ea1388f3bde34a32c4762fde9d6e70d
victor haettinger <elie.lajoinie@supdevinci-edu.fr>
2024-11-23 10:27:27 +0100

    Ajout d'une sécurité

--- src/main/java/com/example/pokemonapi/PokemonApiApplication.java
+++ src/main/java/com/example/pokemonapi/PokemonApiApplication.java
@@ -2,13 +2,15 @@ package com.example.pokemonapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 public class PokemonApiApplication {
 
     public static void main(String[] args) {
